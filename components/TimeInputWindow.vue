5e288e17819c8dd3884206651a683df4f8d8f99a
Atila Ferreira <atila.ferreira13@icloud.com>
2024-09-05 20:58:59 -0300

    fix variable range

--- components/TimeInputWindow.vue
+++ components/TimeInputWindow.vue
@@ -1,48 +1,60 @@
 <template>
-    <div class="flex flex-col justify-start">
-      <span class="text-xl">{{ label }}</span>
-      <div class="flex space-x-2">
-        <input
-          type="number"
-          :value="hours"
-          @input="$emit('update:hours', Number($event.target.value))"
-          class="w-12 border border-emerald-200 bg-transparent rounded px-2 py-1 text-center focus:bg-emerald-200 focus:text-emerald-950 outline-none"
-          placeholder="0"
-          min="0"
-          max="6"
-        />
-        <input
-          type="number"
-          :value="minutes"
-          @input="$emit('update:minutes', Number($event.target.value))"
-          class="w-12 border border-emerald-200 bg-transparent rounded px-2 py-1 text-center focus:bg-emerald-200 focus:text-emerald-950 outline-none"
-          placeholder="0"
-          min="0"
-          max="59"
-        />
-        <input
-          type="number"
-          :value="seconds"
-          @input="$emit('update:seconds', Number($event.target.value))"
-          class="w-12 border border-emerald-200 bg-transparent rounded px-2 py-1 text-center focus:bg-emerald-200 focus:text-emerald-950 outline-none"
-          placeholder="0"
-          min="0"
-          max="59"
-        />
-      </div>
+  <div class="flex flex-col justify-start">
+    <span class="text-xl">{{ label }}</span>
+    <div class="flex space-x-2">
+      <input
+        type="number"
+        :value="hours"
+        @input="$emit('update:hours', Number($event.target.value))"
+        class="w-12 border border-emerald-200 bg-transparent rounded px-2 py-1 text-center focus:bg-emerald-200 focus:text-emerald-950 outline-none"
+        placeholder="0"
+        min="0"
+        max="6"
+      />
+      <input
+        type="number"
+        :value="minutes"
+        @input="$emit('update:minutes', Number($event.target.value))"
+        class="w-12 border border-emerald-200 bg-transparent rounded px-2 py-1 text-center focus:bg-emerald-200 focus:text-emerald-950 outline-none"
+        placeholder="0"
+        min="0"
+        max="59"
+      />
+      <input
+        type="number"
+        :value="seconds"
+        @input="$emit('update:seconds', Number($event.target.value))"
+        class="w-12 border border-emerald-200 bg-transparent rounded px-2 py-1 text-center focus:bg-emerald-200 focus:text-emerald-950 outline-none"
+        placeholder="0"
+        min="0"
+        max="59"
+      />
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      label: String,
-      hours: { type: Number, default: 0 },
-      minutes: { type: Number, default: 0 },
-      seconds: { type: Number, default: 0 },
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    hours: { type: Number, default: 0 },
+    minutes: { type: Number, default: 0 },
+    seconds: { type: Number, default: 0 },
+  },
+  emits: ["update:hours", "update:minutes", "update:seconds"],
+  watch: {
+    hours(newVal) {
+      this.$emit("update:hours", Math.max(0, Math.min(6, newVal)));
     },
-  };
-  </script>
+    minutes(newVal) {
+      this.$emit("update:minutes", Math.max(0, Math.min(59, newVal)));
+    },
+    seconds(newVal) {
+      this.$emit("update:seconds", Math.max(0, Math.min(59, newVal)));
+    },
+  },
+};
+</script>
 
 <style scoped>
 /* Chrome, Safari, Edge, Opera */
